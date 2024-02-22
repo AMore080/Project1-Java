841a39ff7399c3aad2593c4988d81aba2d9a3933
Alexander Moreno <alexmore080@gmail.com>
2024-02-22 13:32:15 -0600

    program functioning

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,81 @@
-import java.io.InputStream;
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         TicketSystem ticketSystem = new TicketSystem();
-        System.out.println("Choose an option: ");
-        System.out.println("1. Enter visitors");
-        System.out.println("2. Start ride queues");
-        System.out.println("3. Exit program");
-        int option = sc.nextInt();
-        switch(option){
-            case 1:
-                System.out.println("How many visitors will ride today?");
-                int num = sc.nextInt();
-                for(int i = 0; i < num; i++){
-                    String nm;
-                    int ag;
-                    String ride;
-                    System.out.println("Enter the name of the visitor: ");
-                    nm = sc.next();
-                    System.out.println(nm);
-                    System.out.println("Enter the age: ");
-                    ag = sc.nextInt();
-                    System.out.println("Select a ride: ");
-                    ride = sc.next();
-                    Visitor visitor = new Visitor(nm, ag, ride);
-                    ticketSystem.purchaseTicket(visitor);
-                    ticketSystem.joinRideQueue(visitor, visitor.getRidePreference());
-                }
-                break;
-        }
+        int option;
 
-//        Visitor visitor1 = new Visitor("Alex", 22, "Roller Coaster");
-//        Visitor visitor2 = new Visitor("Nydia", 17, "Roller Coaster");
+        do
+        {
+            System.out.println("Choose an option: ");
+            System.out.println("1. Enter visitors");
+            System.out.println("2. Start ride queues");
+            System.out.println("3. Exit program");
 
+            option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("How many visitors will ride today?");
+                    int num = sc.nextInt();
+                    for (int i = 0; i < num; i++) {
+                        String nm;
+                        int ag;
+                        String ride = "";
+                        int rideOption;
 
-//        ticketSystem.purchaseTicket(visitor1);
-//        ticketSystem.purchaseTicket(visitor2);
-//        ticketSystem.joinRideQueue(visitor1, visitor1.getRidePreference());
-//        ticketSystem.joinRideQueue(visitor2, visitor2.getRidePreference());
+                        System.out.println("Enter the name of the visitor: ");
+                        nm = sc.next();
 
-        for(String s : ticketSystem.rollerQueue){
+                        System.out.println("Enter the age: ");
+                            while(true){
+                                try {
+                                    ag = sc.nextInt();
+                                    break;
+                                }
+                                catch(InputMismatchException e){
+                                    System.out.print("Enter a valid age: ");
+                                    sc.nextLine();
+                                }
+                            }
+
+                        System.out.println("Select a ride (Enter a number listed): ");
+                            if(ag >= 18){
+                                System.out.println("1. Roller Coaster");
+                                rideOption = sc.nextInt();
+                                    if(rideOption == 1){
+                                        ride = "Roller Coaster";
+                                    }
+                            } else {
+                                System.out.println("1. Kiddie Coaster");
+                                rideOption = sc.nextInt();
+                                    if(rideOption == 1){
+                                        ride = "Kiddie Coaster";
+                                    }
+                            }
+
+                        Visitor visitor = new Visitor(nm, ag, ride);
+                        ticketSystem.purchaseTicket(visitor);
+                        ticketSystem.joinRideQueue(visitor, visitor.getRidePreference());
+                    }
+                    break;
+
+                // Case 2 will start the ride Queues.
+                case 2:
+                    System.out.println("Starting the rides!");
+                    ticketSystem.simulateRides();
+                    break;
+            }
+
+            // Do-while will continue to loop this portion until the user enters the number 3 which will end it.
+        } while (option != 3);
+
+        System.out.println("Bye!");
+
+        for (String s : ticketSystem.rollerQueue) {
             System.out.print(s + " ");
         }
+
     }
 }
--- src/TicketSystem.java
+++ src/TicketSystem.java
@@ -1,32 +1,68 @@
-import java.util.LinkedList;
-import java.util.Objects;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 public class TicketSystem {
     Queue<String> rollerQueue = new LinkedList<>();
+    Queue<String> kiddieQueue = new LinkedList<>();
     Stack<String> ticketStack = new Stack<>();
+
+    // Checks the visitors age and assigns a ticket based on what stage of life they are at
     public void purchaseTicket(Visitor visitor){
         if(visitor.getAge() >= 18){
             visitor.setTicketType("Adult");
+            ticketStack.push(visitor.getName());
         }
         else if (visitor.getAge() < 18){
             visitor.setTicketType("Child");
+            ticketStack.push(visitor.getName());
         }
         else if(visitor.getAge() >= 60){
             visitor.setTicketType("Senior");
+            ticketStack.push(visitor.getName());
         }
     }
 
+    // Will join the ride queue and will not join if the ticket has not been assigned
     public void joinRideQueue(Visitor visitor, String rideName){
         System.out.println("Joining Ride!");
         //Check if the visitor has a ticket, if not it will initiate this if statement.
         if(visitor.getTicketType() == null){
             System.out.println("You have no ticket!");
         }
-        if(Objects.equals(rideName, "RollerCoaster")){
+
+        // These check what option the user selected and then adds them to the queue based on the parameters.
+        if(Objects.equals(rideName, "Roller Coaster") && visitor.getAge() >= 18){
             rollerQueue.offer(visitor.getName());
+        }
+
+        if(Objects.equals(rideName, "Kiddie Coaster") && visitor.getAge() < 18){
+            System.out.println("Sorry, to the Kiddie Coaster you go.");
+            kiddieQueue.offer(visitor.getName());
         }
     }
 
+    public void simulateRides(){
+        // Will loop until the specified ride queue is empty. It will remove the head of the queue and then sleep for a few
+        // seconds before continuing with the next element.
+        while(!rollerQueue.isEmpty()){
+            String rider = rollerQueue.poll();
+            System.out.println(rider + " is riding the Roller Coaster!");
+
+            try {
+                Thread.sleep(new Random().nextInt(5) * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        while(!kiddieQueue.isEmpty()){
+            String kidRider = kiddieQueue.poll();
+            System.out.println(kidRider + " is riding the Kiddie Coaster!");
+
+            try {
+                Thread.sleep(new Random().nextInt(5) * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
